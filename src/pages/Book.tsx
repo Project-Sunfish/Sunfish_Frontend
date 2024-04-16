5eacb10ca3cacce94682a0b70d38c8ce5bbc419a
0xseo <0xseo94@gmail.com>
2024-04-17 02:31:32 +0900

    FEEAT/ Vibration Trial

--- src/pages/Book.tsx
+++ src/pages/Book.tsx
@@ -1,9 +1,12 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Pressable, StyleSheet, Text, Vibration, View} from 'react-native';
 import {Ex} from '../components/animations';
 
 export default function Book() {
   return (
     <View style={{flex: 1}}>
+      <Pressable onPress={() => Vibration.vibrate(100)}>
+        <Text>Haptic</Text>
+      </Pressable>
       <Text>Book</Text>
       <Ex />
     </View>
