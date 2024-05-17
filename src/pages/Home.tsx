e5bbee1abf9e383fef1f1f0b55374c1a2cc9d748
0xseo <0xseo94@gmail.com>
2024-05-17 21:06:18 +0900

    UI/ Create Btn, Modal, Category select, worry input modal

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,19 +3,38 @@ import {
   ImageBackground,
   Pressable,
   StyleSheet,
+  TextInput,
   View,
 } from 'react-native';
 import Text from '../components/Text';
 import Character from '../components/Character';
-import {useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import MyPageModal from '../components/MyPageModal';
 import {SvgXml} from 'react-native-svg';
 import {svgList} from '../assets/svgList';
 
 export default function Home() {
   const [cnt, setCnt] = useState(0);
-  const [newBogus, setNewBogus] = useState(false);
+  const [newBogus, setNewBogus] = useState(true);
   const [modal, setModal] = useState('no');
+  const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
+  const [worry, setWorry] = useState('');
+
+  const formatDate = (date: Date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작하므로 +1
+    const day = String(date.getDate()).padStart(2, '0');
+    const weekDays = ['일', '월', '화', '수', '목', '금', '토'];
+    const dayOfWeek = weekDays[date.getDay()];
+
+    return `${year}.${month}.${day}(${dayOfWeek})`;
+  };
+
+  const ref = useRef<TextInput>(null);
+  useEffect(() => {
+    ref.current?.setNativeProps({style: {fontFamily: 'handWriting'}});
+  });
+
   return (
     <ImageBackground
       source={require('../assets/pictures/Base.png')}
@@ -88,6 +107,140 @@ export default function Home() {
           <Text style={styles.cannotCreateBtnTxt}>닫기</Text>
         </Pressable>
       </MyPageModal>
+      <MyPageModal
+        showModal={modal}
+        setShowModal={setModal}
+        condition="selectCategory"
+        headerTxt={`걱정의 종류를\n‘최대 3개’\n선택해주세요`}>
+        <SvgXml xml={svgList.termModal.separator} style={{marginTop: 8}} />
+        <View
+          style={{
+            width: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <FlatList
+            style={styles.selectCategoryView}
+            data={[
+              '학업',
+              '직장',
+              '가족',
+              '친구',
+              '연인',
+              '건강',
+              '사회문제',
+              '이유없음',
+              '기타',
+            ]}
+            renderItem={({item, index}) => (
+              <Pressable
+                style={[
+                  styles.cateBtn,
+                  selectedCategory.includes(item) && {
+                    borderWidth: 1,
+                    borderColor: '#002B5D',
+                  },
+                ]}
+                onPress={() => {
+                  if (selectedCategory.includes(item)) {
+                    setSelectedCategory(
+                      selectedCategory.filter(e => e !== item),
+                    );
+                  } else {
+                    if (selectedCategory.length <= 2) {
+                      setSelectedCategory([...selectedCategory, item]);
+                    }
+                  }
+                }}>
+                <Text
+                  style={[
+                    styles.cateBtnTxt,
+                    selectedCategory.includes(item) && {
+                      color: '#002B5D',
+                    },
+                  ]}>
+                  {item}
+                </Text>
+              </Pressable>
+            )}
+            keyExtractor={(item, index) => index.toString()}
+            numColumns={3}
+          />
+        </View>
+        <View style={styles.selectCategoryBtnView}>
+          <Pressable
+            style={styles.selectCategoryBtn}
+            onPress={() => {
+              setModal('no');
+              setSelectedCategory([]);
+            }}>
+            <Text style={styles.cannotCreateBtnTxt}>취소</Text>
+          </Pressable>
+          <View style={{width: 16}}></View>
+          <Pressable
+            disabled={selectedCategory.length < 1}
+            onPress={() => setModal('worry')}
+            style={[
+              styles.selectCategoryBtn,
+              selectedCategory.length >= 1 && {backgroundColor: '#6EA5FF'},
+            ]}>
+            <Text
+              style={[
+                styles.cannotCreateBtnTxt,
+                selectedCategory.length >= 1 && {color: '#FFFFFF'},
+              ]}>
+              다음
+            </Text>
+          </Pressable>
+        </View>
+      </MyPageModal>
+      <MyPageModal
+        showModal={modal}
+        setShowModal={setModal}
+        condition="worry"
+        headerTxt="어떤 걱정인가요?">
+        <View style={styles.worryContent}>
+          <Text style={styles.date}>{formatDate(new Date())}</Text>
+          <TextInput
+            style={[
+              styles.worryInput,
+              worry.trim().length >= 1 && {paddingBottom: 30},
+            ]}
+            placeholder="오늘 하루를 기록해보세요."
+            placeholderTextColor={'#002B5D80'}
+            value={worry}
+            onChangeText={text => setWorry(text)}
+            multiline={true}
+            ref={ref}
+          />
+          {worry.trim().length >= 1 && (
+            <Text style={styles.quantity}>{`${worry.length}/1000`}</Text>
+          )}
+        </View>
+        <View style={styles.selectCategoryBtnView}>
+          <Pressable
+            style={styles.selectCategoryBtn}
+            onPress={() => setModal('selectCategory')}>
+            <Text style={styles.cannotCreateBtnTxt}>취소</Text>
+          </Pressable>
+          <View style={{width: 16}}></View>
+          <Pressable
+            disabled={worry.trim().length < 1}
+            // onPress={() => setModal('worry')}
+            style={[
+              styles.selectCategoryBtn,
+              worry.trim().length >= 1 && {backgroundColor: '#6EA5FF'},
+            ]}>
+            <Text
+              style={[
+                styles.cannotCreateBtnTxt,
+                worry.trim().length >= 1 && {color: '#FFFFFF'},
+              ]}>
+              다음
+            </Text>
+          </Pressable>
+        </View>
+      </MyPageModal>
     </ImageBackground>
   );
 }
@@ -153,5 +306,68 @@ const styles = StyleSheet.create({
     color: '#002B5D',
     fontSize: 13,
     fontWeight: '400',
+  },
+  selectCategoryView: {
+    flexDirection: 'row',
+    marginBottom: 12,
+    marginTop: 23,
+  },
+  cateBtn: {
+    flex: 1,
+    width: 60,
+    height: 60,
+    margin: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 7,
+    backgroundColor: '#E6EEF7',
+  },
+  cateBtnTxt: {
+    color: '#002B5D80',
+    fontSize: 11,
+    fontWeight: '400',
+  },
+  selectCategoryBtnView: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 30,
+  },
+  selectCategoryBtn: {
+    backgroundColor: '#4F85C54D',
+    borderRadius: 20,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+  },
+  worryContent: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+  },
+  date: {
+    marginVertical: 12,
+    color: '#6EA5FF',
+    fontSize: 11,
+    fontWeight: '400',
+  },
+  worryInput: {
+    backgroundColor: '#E6EEF7',
+    borderRadius: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    width: '100%',
+    marginBottom: 12,
+    textAlign: 'center',
+    maxHeight: 220,
+  },
+  quantity: {
+    color: '#002B5D80',
+    fontSize: 8,
+    fontWeight: '400',
+    position: 'absolute',
+    bottom: 20,
+    right: 7,
   },
 });
